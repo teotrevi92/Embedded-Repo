6f6f7ab42f1b2c2d495a3de028baad8fd3d961b0
marcotritoni <marco.tritoni@gmail.com>
2015-06-11 02:18:12 +0200

    per chi vuole lavorare da questo commit può, risolto un problema nell'evidenziazione della session in corso

--- FallDetector/src/com/esp1415NONE/falldetector/classi/SessionSimpleCursorAdapter.java
+++ FallDetector/src/com/esp1415NONE/falldetector/classi/SessionSimpleCursorAdapter.java
@@ -47,28 +47,30 @@ public class SessionSimpleCursorAdapter extends SimpleCursorAdapter
 		MyGraph rndBitmap = new MyGraph(size,size);
 		rndBitmap.doRandomImg(dateA[0], dateA[1], dateA[2], dateA[3], dateA[4], dateA[5], size);
 		logo.setImageBitmap(rndBitmap.getRandomImg());
-		
-		
-//		for (int i = 0; i < getListView().getAdapter().getCount(); i++) {
+
+
+		//		for (int i = 0; i < getListView().getAdapter().getCount(); i++) {
 
 		//COME IDENTIFICARE LA SESSIONE CORRENTE REGISTRATA E EVIDENZIARE LA RIGA NELLA LISTVIEW
 		TextView ids = (TextView) view.findViewById(R.id.nome);
 		TextView nomeS = (TextView) view.findViewById(R.id.nomeS);
 		String idss = ids.getText().toString();
 		if((idss.equals(idsC)) && ((isPlaying == 1) || (isPlaying == -1))) {
-//			Toast.makeText(context, "la sessione corrent e' " + idss, Toast.LENGTH_SHORT).show();
-//			v.setBackgroundColor(Color.RED);
-//			textview.setTextColor(Color.RED);
-//			textview.setTypeface(textview.getTypeface(), Typeface.BOLD);
-//			rec.setVisibility(View.VISIBLE);
-//			view.setBackgroundColor(Color.RED);
+			//			Toast.makeText(context, "la sessione corrent e' " + idss, Toast.LENGTH_SHORT).show();
+			//			v.setBackgroundColor(Color.RED);
+			//			textview.setTextColor(Color.RED);
+			//			textview.setTypeface(textview.getTypeface(), Typeface.BOLD);
+			//			rec.setVisibility(View.VISIBLE);
+			//			view.setBackgroundColor(Color.RED);
 			ids.setTextColor(Color.RED);
 			nomeS.setTextColor(Color.RED);
-			
-			
 		}
-		//	}
-		
+		else {
+			ids.setTextColor(Color.GRAY);
+			nomeS.setTextColor(Color.GRAY);
+		}
+
+
 
 	}
 
