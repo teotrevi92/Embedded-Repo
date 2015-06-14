bb09d0a31be3dfe4ea797fa2b96940a869aef239
marcotritoni <marco.tritoni@gmail.com>
2015-06-14 14:49:45 +0200

    messaggio di conferma per la gestione database. Ora tutti gli inserimenti hanno anche la prima letterea maiuscola di default

--- FallDetector/src/com/esp1415NONE/falldetector/FragmentSettings.java
+++ FallDetector/src/com/esp1415NONE/falldetector/FragmentSettings.java
@@ -4,18 +4,16 @@ package com.esp1415NONE.falldetector;
 
 import java.util.Calendar;
 
-
-
-
-
 import com.esp1415NONE.falldetector.classi.DbAdapter;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.app.AlarmManager;
+import android.app.Dialog;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -23,9 +21,12 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -38,11 +39,11 @@ public class FragmentSettings extends Fragment {
 	private TimePicker tmpicker;
 	private PendingIntent pendIntent;
 	private AlarmManager alarmManager;
-	private Button button,btSession, btData; //Tri
+	private Button button, btSession, btData; //Tri
 	private FragmentTransaction fragmentTransaction;
 	private FragmentManager fragmentManager;
 	private DbAdapter dbAdapter;
-	//	private DbAdapter dbAdapter; //Tri
+	private int isOpenDialog = 0; //1 se cancella sessioni, 2 se cancella tutto, 0 se non ci sono Dialog aperti
 
 	View view;
 	@Override
@@ -50,6 +51,13 @@ public class FragmentSettings extends Fragment {
 		// TODO Auto-generated method stub
 		view = inflater.inflate(R.layout.activity_fragment_settings, container, false);
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		isOpenDialog = preferences.getInt("dialog", 0);
+		if(isOpenDialog == 1) {
+			dialogDropSession();
+		}
+		else if(isOpenDialog == 2) {
+			dialogDropData();
+		}
 		int int_sp_acc = preferences.getInt("spinnerValueAcc", 1);
 		int int_sp_duration = preferences.getInt("spinnerValueDuration", 1);
 		int int_sp_sens = preferences.getInt("spinnerValueSens", 1);
@@ -110,8 +118,9 @@ public class FragmentSettings extends Fragment {
 				}
 				else {
 
-					dbAdapter.dropAllSession();
-					Toast.makeText(getActivity(),"Cancellate tutte le sessioni", Toast.LENGTH_SHORT).show();
+					//					dbAdapter.dropAllSession();
+					//					Toast.makeText(getActivity(),"Cancellate tutte le sessioni", Toast.LENGTH_SHORT).show();
+					dialogDropSession();
 				}
 			}
 		});
@@ -127,9 +136,9 @@ public class FragmentSettings extends Fragment {
 
 				}
 				else {
-
-					dbAdapter.dropAllData();
-					Toast.makeText(getActivity(),"Cancellati tutti i dati", Toast.LENGTH_SHORT).show();
+					dialogDropData();
+//					dbAdapter.dropAllData();
+//					Toast.makeText(getActivity(),"Cancellati tutti i dati", Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
@@ -196,7 +205,12 @@ public class FragmentSettings extends Fragment {
 		boolean bln_cbt = cbt.isChecked();
 		int hour = tmpicker.getCurrentHour();
 		int min = tmpicker.getCurrentMinute();
-
+		if(getActivity().isFinishing()) {
+			editor.putInt("dialog", 0);
+		}
+		else {
+			editor.putInt("dialog", isOpenDialog);
+		}
 
 
 		//Salvataggio impostazioni
@@ -209,6 +223,114 @@ public class FragmentSettings extends Fragment {
 		//facciamo il commit
 		editor.commit();
 
+	}
+
+	private void dialogDropSession() {
+		final Dialog dialog = new Dialog(getActivity());
+		dialog.setContentView(R.layout.confirm);
+		dialog.setTitle("Gestione Dati Database");
+		TextView text;
+		dialog.setOnCancelListener(new DialogInterface.OnCancelListener()
+		{@Override
+			public void onCancel(DialogInterface dialog)
+		{
+			isOpenDialog = 0;
+			dialog.dismiss();
+		}
+		});
+
+		//personalizzo il Dialog
+		text = (TextView) dialog.findViewById(R.id.text);
+		text.setText("Vuoi cancellare tutte le sessioni con relative cadute dal Database?");
+		isOpenDialog = 1;
+		Button ok = (Button) dialog.findViewById(R.id.btn_yes);
+		Button no = (Button) dialog.findViewById(R.id.btn_no);
+
+		// cosa faccio al click del conferma
+		ok.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				dbAdapter.dropAllSession();
+				isOpenDialog = 0;
+				Toast.makeText(getActivity(),"Cancellate tutte le sessioni", Toast.LENGTH_SHORT).show();
+				dialog.dismiss();
+
+			}
+		});
+		// cosa faccio al click dell'annulla
+		no.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				isOpenDialog = 0;
+				//				inDialog = false;
+				dialog.dismiss();
+
+			}
+		});
+		dialog.show();
+	}
+	
+	private void dialogDropData() {
+		final Dialog dialog = new Dialog(getActivity());
+		dialog.setContentView(R.layout.confirm);
+		dialog.setTitle("Gestione Dati Database");
+		TextView text;
+		dialog.setOnCancelListener(new DialogInterface.OnCancelListener()
+		{@Override
+			public void onCancel(DialogInterface dialog)
+		{
+			isOpenDialog = 0;
+			dialog.dismiss();
+		}
+		});
+
+		//personalizzo il Dialog
+		text = (TextView) dialog.findViewById(R.id.text);
+		text.setText("Vuoi cancellare tutti i dati del Database?");
+		isOpenDialog = 2;
+		Button ok = (Button) dialog.findViewById(R.id.btn_yes);
+		Button no = (Button) dialog.findViewById(R.id.btn_no);
+
+		// cosa faccio al click del conferma
+		ok.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				dbAdapter.dropAllData();
+				isOpenDialog = 0;
+				Toast.makeText(getActivity(),"Cancellate tutti i dati", Toast.LENGTH_SHORT).show();
+				dialog.dismiss();
+
+			}
+		});
+		// cosa faccio al click dell'annulla
+		no.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				isOpenDialog = 0;
+				//				inDialog = false;
+				dialog.dismiss();
+
+			}
+		});
+		dialog.show();
+	}
+	
+	@Override
+	public void onDestroyView() {
+		// TODO Auto-generated method stub
+		super.onDestroyView();
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		SharedPreferences.Editor editor = preferences.edit();
+		if(getActivity().isFinishing())
+			editor.putInt("dialog", 0);
+		//			c.close();
+		//		}
+		//facciamo il commit
+		editor.commit();
 	}
 
 
